546c057c16d0bb7f3b52356978d0aec9e3b13d18
shashi-w3vila <shashi.prakash@w3villa.com>
2021-12-13 12:20:50 +0530

    Create a feature to mark todo_item as completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
+
   root "todo_lists#index"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
